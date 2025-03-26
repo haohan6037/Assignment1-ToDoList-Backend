4b46cfa0e34451363aa7979d47fa15c3beec7690
haohan6037 <haohan6037@gmail.com>
2025-03-26 16:21:27 +1300

    git fetch origin

--- todolist_backend_s12025/settings.py
+++ todolist_backend_s12025/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'todolist_backend_s12025.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'neondb',
+        'USER': 'neondb_owner',
+        'PASSWORD': 'npg_4KtCozMaL8NJ',
+        'HOST': 'ep-morning-sun-a7712nbb-pooler.ap-southeast-2.aws.neon.tech',
+        'PORT': '5432',
     }
 }
 
